41b9093b63e18e832dfe5e1add9a8dc275a929e9
Daniil Shcherbakov <burhaunt@gmail.com>
2019-06-18 01:50:28 +0300

    Test

--- src/components/admin/tables/user/UserEditDialog.vue
+++ src/components/admin/tables/user/UserEditDialog.vue
@@ -2,7 +2,7 @@
     <v-dialog v-model="isOpen" max-width="500px" persistent lazy>
         <v-card>
             <v-card-title>
-                <span class="headline mt-2 ml-2">{{ itemName }}</span>
+                <span class="headline mt-2 ml-2"> Пользователь </span>
             </v-card-title>
 
             <v-card-text>
